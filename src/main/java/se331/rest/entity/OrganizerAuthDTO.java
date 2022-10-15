29cb51bef9f54bbaf4401783d64946cb9c3496bb
POOM <nitipoom_t@cmu.ac.th>
2022-10-15 17:45:26 +0700

    6.8

---
+++ src/main/java/se331/rest/entity/OrganizerAuthDTO.java
@@ -0,0 +1,21 @@
+package se331.rest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import se331.rest.security.entity.Authority;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class OrganizerAuthDTO {
+    long id;
+    String name;
+    List<String> authorities;
+
+}
